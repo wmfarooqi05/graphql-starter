80678e09eb543694a2a2bf9b64ed310b027e4882
Waleed Farooqi <wmfarooqi05@gmail.com>
2019-09-19 15:19:18 +0500

    Get api with two types of data fields in schema

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 import express from 'express';
+import expressGraphQL from 'graphql';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  return res.json({
+    msg: 'Welcome to GraphQL World'
+  })
+})
+
+root = {
+  item: () => {
+    return {
+      id: 1,
+      timeISO: '2 pm tuesday',
+      time: '123432',
+      text: 'this is hacker news item',
+      title: 'Welcome to GraphQL',
+      deleted: false
+    }
+  },
+  user: () => {
+    return {
+      firstName: 'Jane',
+      lastName: 'Doe',
+      emails: [
+        {
+          email: 'jane@gmail.com',
+        },
+        {
+          email: 'jane@yahoo.com',
+        }
+      ]
+    }
+  }
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
 app.listen(PORT, () => {
-    console.log(`App is running on ${PORT}`);
+  console.log(`App is running on ${PORT}`);
 })
---
+++ schema.js
@@ -0,0 +1,30 @@
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+
+  type HackerNewsItem {
+    id: String,
+    text: String,
+    timeISO: String,
+    time: String,
+    title: String,
+    deleted: Boolean
+  }
+
+  type Email {
+    email: String!
+  }
+
+  type User {
+    firstName: String!,
+    lastName: String!,
+    emails: [Email]
+  }
+
+  type Query {
+    item: HackerNewsItem,
+    user: User
+  }
+`);
+
+export default schema;
